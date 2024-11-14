2f189c79c35ceae76ad759d207319500a9168656
DO VAN HUNG <dovanhung25102003glhd@gmail.com>
2024-11-14 10:51:48 +0700

    project

---
+++ Controllers/thongtinController.cs
@@ -0,0 +1,82 @@
+﻿using kien.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace kien.Controllers
+{
+    public class thongtinController : Controller
+    {
+          List<nhanvien> li = new List<nhanvien>();
+        List<nhanvien> li1 = new List<nhanvien>();
+        List<nhanvien> li2 = new List<nhanvien>();
+        // GET: thongtin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public thongtinController()
+        {
+            li.Add(new nhanvien("hoàng" ,"kiên","Nam",2003,1,"music",200,25));
+            li.Add(new nhanvien("hoàng", "văn", "Nam", 2003, 1, "music", 200, 25));
+            li.Add(new nhanvien("nguyễn", "linh", "Nữ", 2003, 1, "touris", 150, 20));
+            li.Add(new nhanvien("hoàng", "hồng", "Nam", 2003, 1, "music", 170, 25));
+            li.Add(new nhanvien("hoàng", "kiên", "Nam", 2003, 1, "music", 400, 25));
+            li.Add(new nhanvien("ly", "nam", "Nữ", 2003, 1, "music", 500, 25));
+            li.Add(new nhanvien("hoàng", "trân", "Nam", 2005, 1, "music", 200, 15));
+
+            li.Add(new nhanvien("bui", "huan", "Nam", 2003, 1, "music", 300, 20));
+        }
+
+        public ActionResult nhap( nhanvien nv)
+        {      ViewBag.firstname = nv.firstname;
+            ViewBag.firstname = nv.firstname;
+            ViewBag.lastname = nv.lastname;
+            ViewBag.gioitinh = nv.gioitinh;
+            ViewBag.namsinh = nv.namsinh;
+            ViewBag.thangsinh = nv.thangsinh;
+            ViewBag.sothich = nv.sothich;
+            ViewBag.salary = nv.salary;
+            ViewBag.ngaylv = nv.ngaylv;
+            ViewBag.luongthuc = nv.luongthuc();
+            return View();
+        }
+        public ActionResult xuat(nhanvien nv)
+        {
+            ViewBag.firstname = nv.firstname;
+            ViewBag.firstname = nv.firstname;
+            ViewBag.lastname = nv.lastname;
+            ViewBag.gioitinh = nv.gioitinh;
+            ViewBag.namsinh = nv.namsinh;
+            ViewBag.thangsinh = nv.thangsinh;
+            ViewBag.sothich = nv.sothich;
+            ViewBag.salary = nv.salary;
+            ViewBag.ngaylv = nv.ngaylv;
+            ViewBag.luongthuc = nv.luongthuc();
+            return View();
+        }
+        public ActionResult xuatlist(nhanvien nv)
+        {
+            foreach( var item in li)
+            {
+                if(item.salary >300)
+                {
+                    li1.Add(item);
+                }
+            }
+            foreach (var item in li)
+            {
+                if (item.gioitinh =="Nam")
+                {
+                    li2.Add(item);
+                }
+            }
+            ViewBag.li1 = li1;
+            ViewBag.li2 = li2;
+            ViewBag.li = li;
+            return View();
+        }
+    }
+}
---
+++ Models/nhanvien.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace kien.Models
+{
+    public class nhanvien
+    {
+        public nhanvien()
+        {
+        }
+
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string gioitinh { get; set; }
+        public double namsinh { get; set; }
+
+        public int thangsinh { get; set; }
+        public string sothich { get; set; }
+        public double salary { get; set; }
+        public int ngaylv { get; set; }
+
+        public nhanvien(string firstname, string lastname, string gioitinh, double namsinh, int thangsinh, string sothich, double salary, int ngaylv)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.gioitinh = gioitinh;
+            this.namsinh = namsinh;
+            this.thangsinh = thangsinh;
+            this.sothich = sothich;
+            this.salary = salary;
+            this.ngaylv = ngaylv;
+        }
+
+        public double luongthuc()
+        {   
+            if(ngaylv >= 25)
+            {
+                return salary * 1.1;
+            }
+           else if (ngaylv >= 20)
+            {
+                return salary * 1.05;
+            }
+             else if (ngaylv < 20)
+            {
+                return salary;
+            }
+            else
+            {
+                return 0;
+            }
+            
+        }
+    }
+}
